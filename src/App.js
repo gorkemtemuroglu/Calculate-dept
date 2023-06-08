c061bed17e3e9d7468948037a41bc088949178a5
Görkem <gorkem.temuroglu@icloud.com>
2023-06-08 16:27:09 +0300

    Added Key Prop

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ export default function App() {
 
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
